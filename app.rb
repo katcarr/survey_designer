f20d943f6fd7d971b18f259fbdc7240511514029
Mike Harris & Kathryn Carr <student@epicodus.com>
2015-01-28 15:25:38 -0800

    Add ability to edit and delete surveys and their questions

--- app.rb
+++ app.rb
@@ -28,5 +28,48 @@ post('/questions') do
   @survey = Survey.find(survey_id)
   Question.create({:description => description, :survey_id => survey_id})
   @questions= Question.all()
-erb(:survey)
+  erb(:survey)
+end
+
+get('/edit_question/:id') do
+  @question = Question.find(params.fetch("id").to_i())
+  erb(:edit_question)
+end
+
+patch('/edit_question') do
+  new_description = params.fetch("new_description")
+  question_id = params.fetch("question_id").to_i()
+  @question = Question.find(question_id)
+  @question.update({:description => new_description})
+  erb(:edit_question)
+end
+
+delete('/delete_question') do
+  question_id= params.fetch("question_id").to_i()
+  @question= Question.find(question_id)
+  @survey= Survey.find(@question.survey_id())
+  @question.destroy()
+  @questions= Question.all()
+  erb(:survey)
+end
+
+get('/edit_survey/:id') do
+  @survey= Survey.find(params.fetch("id").to_i())
+  erb(:edit_survey)
+end
+
+patch('/edit_survey') do
+  new_name= params.fetch("new_name")
+  survey_id= params.fetch("survey_id").to_i()
+  @survey= Survey.find(survey_id)
+  @survey.update({:name => new_name})
+  erb(:edit_survey)
+end
+
+delete('/delete_survey') do
+  survey_id= params.fetch("survey_id").to_i()
+  @survey= Survey.find(survey_id)
+  @survey.destroy()
+  @surveys= Survey.all()
+  erb(:index)
 end
--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -2,7 +2,7 @@ require('spec_helper')
 
 describe(Survey) do
 
-  # it {should have_many(:questions)}
+  it {should have_many(:questions)}
 
 
   describe("#upcase_title") do
