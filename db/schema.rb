7c3ec848cc8ce4d7cf29f199dce268ea9ee4f8fc
Mike Harris & Kathryn Carr <student@epicodus.com>
2015-01-28 10:57:25 -0800

    Add Question class and table in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128173128) do
+ActiveRecord::Schema.define(version: 20150128185105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "survey_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "surveys", force: :cascade do |t|
     t.string   "name"
