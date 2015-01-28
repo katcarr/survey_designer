75e5c8180cc8ce3d3dda0e91b644ceacd85ed2cb
Mike Harris & Kathryn Carr <student@epicodus.com>
2015-01-28 09:45:36 -0800

    Add survey class

--- app.rb
+++ app.rb
@@ -1,2 +1,4 @@
 require("bundle/setup")
 Bundler.require(:default)
+
+DIR[File.dirname(_FILE_) + '/lib/*.rb'].each { |file| require file}
--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -2,3 +2,15 @@ ENY['RACK_ENV'] = 'test'
 
 require('bundler/setup')
 Bundler.require(:default)
+
+
+Dir[File.dirname(_FILE_) + "/../lib/*.rb"].each { |file| require file}
+
+
+Rspec.configure do |config|
+  config.after(:each) do
+    Survey.all().each do |survey|
+      survey.destroy()
+    end
+  end
+end
